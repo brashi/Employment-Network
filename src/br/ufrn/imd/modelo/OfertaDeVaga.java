717d687583b598093515ebdb5be542013295780f
lauro <lauro@DESKTOP-V2274JP>
2021-11-16 01:26:00 -0300

    Comentários adicionados

--- src/br/ufrn/imd/modelo/OfertaDeVaga.java
+++ src/br/ufrn/imd/modelo/OfertaDeVaga.java
@@ -24,14 +24,23 @@ public class OfertaDeVaga {
 		listaDeDocumentos = new ArrayList<String>();
 	}
 	
+	/**
+	 * @return pegar o array de candidatos
+	 */
 	public ArrayList<Candidato> getCandidatos() {
 		return candidatos;
 	}
 	
+	/**
+	 * @param adicionar candidato no array
+	 */
 	public void addCandidato(Candidato candidato) {
 		candidatos.add(candidato);
 	}
 	
+	/**
+	 * @param remover candidato do array
+	 */
 	public void remCandidato(Candidato candidato) {
 		candidatos.remove(candidato);
 	}
@@ -40,10 +49,16 @@ public class OfertaDeVaga {
 	}
 	
 	
+	/**
+	 * @param adicionar uma prova ao array
+	 */
 	public void addProva(Prova prova) {
 		listaDeProvas.add(prova);
 	}
 	
+	/**
+	 * @param remove a prova do array
+	 */
 	public void remProva(Prova prova) {
 		listaDeProvas.remove(prova);
 	}
@@ -51,10 +66,18 @@ public class OfertaDeVaga {
 	public void listarProvas() {
 	}
 	
+	
+	/**
+	 * @param adicionar um documento ao array
+	 */
 	public void addDocumento(String documento) {
 		listaDeDocumentos.add(documento);
 	}
 	
+	
+	/**
+	 * @param remove o documento do array
+	 */
 	public void remDocumento(String documento) {
 		listaDeDocumentos.remove(documento);
 	}
@@ -62,36 +85,60 @@ public class OfertaDeVaga {
 	public void listarDocumentos() {
 	}
 	
+	
+	/**
+	 * @return nome da vaga
+	 */
 	public String getNome() {
 		return nome;
 	}
 	
+	/**
+	 * @param nome da vaga
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
+	/**
+	 * @return descrição da vaga
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
 	
+	/**
+	 * @param descricao da vaga
+	 */
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 	
-	
+	/**
+	 * @return empresa
+	 */
 	public Empresa getEmpresa() {
 		return empresa;
 	}
 	
+	/**
+	 * @param empresa
+	 */
 	public void setEmpresa(Empresa empresa) {
 		this.empresa = empresa;
 	}
 	
-	
+	/**
+	 * @return estado da prova (inscrições abertas, fechadas ou finalizada)
+	 */
 	public status getEstado() {
 		return estado;
 	}
 	
+	
+	/**
+	 * @param estado da prova
+	 */
 	public void setEstado(status estado) {
 		this.estado = estado;
 	}
